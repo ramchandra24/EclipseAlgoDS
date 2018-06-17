d640809edd87d9a71bfd2ee07b0a0a351b9e0286
RAM <RAM@ASUS-PC>
2018-06-17 23:14:46 +0530

    initial commit

---
+++ AlgoGitExample/src/AlgoGitExample.cpp
@@ -0,0 +1,52 @@
+//============================================================================
+// Name        : AlgoGitExample.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+typedef struct node{
+	int data;
+	struct node * left;
+	struct node * right;
+}NODE;
+
+bool isBSTUtil(NODE * root, int min, int max){
+
+	if(NULL == root)
+		return true;
+
+	if(root->data < min || root->data > max)
+		return false;
+
+	return (isBSTUtil(root->left, min, root->data) & isBSTUtil(root->right, max, root->data));
+}
+
+bool isBST(NODE * root){
+	return isBSTUtil(root, INT_MIN, INT_MAX);
+}
+
+NODE * addNode(int data){
+	NODE * nd = new NODE;
+	nd->data = data;
+	nd->left = NULL;
+	nd->right = NULL;
+	return nd;
+}
+
+int main(){
+	NODE * root = addNode(2);
+	root->left = addNode(5);
+	root->right = addNode(6);
+
+	cout << "isBST : " << isBST(root) << endl;
+
+	return 0;
+}
+
