eed0e5b86f167af3d6e35f15bebc8d2d35d0a6a1
Ram <r.kulkarni@ufl.edu>
2018-12-20 15:59:05 -0500

    Using exponents instead of subtraction to find quotient

--- AlgoCppPrac/src/IntegerDivision.cpp
+++ AlgoCppPrac/src/IntegerDivision.cpp
@@ -5,34 +5,31 @@ using namespace std;
 class Solution {
 public:
     int divide(int dividend, int divisor) {
+        int sign = (dividend < 0) ^ (divisor < 0) ? -1 : 1;
+        dividend = abs(dividend);
+        divisor = abs(divisor);
 
-    }
-
-    bool divideBySub(int divi, int div, int odiv, int& q) {
-        if (divi - div <= odiv) {
-            if (divi - div == odiv){
-                ++q;
+        unsigned long long q = 0;
+        unsigned long long temp = 0L;
+        for(int i = 31; i >= 0; --i) {
+            if ((temp + ((unsigned long long)(divisor) << i)) <= dividend) {
+                cout << "i : " << i << endl;
+                temp += (unsigned long long)(divisor << i);
+                q |= 1 << i;
             }
-            if (divi - div >= 0) {
-                return true;
-            }
-            return false;
         }
-        q += q;
-        if (divideBySub(divi, div+div, odiv, q)) {
-            return true;
+        cout << q << endl;
+        if (q > INT_MAX) {
+            q = INT_MAX;
         }
-        q -= (q >> 2);
-        return divideBySub(divi, (div + (div>>1)), odiv, q);
-
+        return sign * q;
     }
-
 };
 
 int main() {
     Solution s;
     int q = 1;
-    cout << s.divideBySub(100, 3, 3, q) << endl;
+    cout << s.divide(INT_MIN, -1) << endl;
     cout << "qu : " << q << endl;
     return 0;
 }
