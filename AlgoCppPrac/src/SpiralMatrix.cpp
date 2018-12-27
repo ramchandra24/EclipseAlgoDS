51cdbc24310a7e8dbd93854b332705689d8ad745
Ram <r.kulkarni@ufl.edu>
2018-12-26 22:20:07 -0500

    Printing matrix in spiral form

--- AlgoCppPrac/src/SpiralMatrix.cpp
+++ AlgoCppPrac/src/SpiralMatrix.cpp
@@ -7,29 +7,38 @@ public:
         vector<int> res;
         int row = matrix.size();
         bool l, r, d;
-        for (int i = 0; i <= row / 2; ++i) {
-            int col = matrix[i].size();
-            l = r = d = false;
-            for (int j = i; j < col - i; ++j) {
-                res.push_back(matrix[i][j]);
-                l = true;
+        if (row > 0) {
+            int times;
+            if (row % 2 == 0) {
+                times = row / 2;
             }
-            if (l) {
-                for (int j = i + 1; j < row - i; ++j) {
-                    res.push_back(matrix[j][col - i - 1]);
-                    d = true;
+            else {
+                times = row / 2 + 1;
+            }
+            for (int i = 0; i < times; ++i) {
+                int col = matrix[i].size();
+                l = r = d = false;
+                for (int j = i; j < col - i; ++j) {
+                    res.push_back(matrix[i][j]);
+                    l = true;
                 }
-            }
-            if (d) {
-                for (int j = col - i - 2; j >= i; --j) {
-                    res.push_back(matrix[row - i - 1][j]);
-                    r = true;
+                if (l) {
+                    for (int j = i + 1; j < row - i; ++j) {
+                        res.push_back(matrix[j][col - i - 1]);
+                        d = true;
+                    }
                 }
-            }
+                if (d) {
+                    for (int j = col - i - 2; j >= i; --j) {
+                        res.push_back(matrix[row - i - 1][j]);
+                        r = true;
+                    }
+                }
 
-            if (r) {
-                for (int j = row - i - 2; j > i ; --j) {
-                    res.push_back(matrix[j][i]);
+                if (r) {
+                    for (int j = row - i - 2; j > i ; --j) {
+                        res.push_back(matrix[j][i]);
+                    }
                 }
             }
         }
@@ -55,9 +64,12 @@ public:
 
 int main() {
     Solution s;
-    vector<vector<int>> m = {{1, 2, 3, 4},
-            {5, 6, 7, 8},
-            {9,10,11,12}};
+    vector<vector<int>> m = {
+                              { 1, 2, 3 },
+                              {4, 6, 8},
+                              {6, 2, 0},
+                              {3, 2, 6}
+                             };
     vector<int> res = s.spiralOrder(m);
     s.printvec(res);
     return 0;
