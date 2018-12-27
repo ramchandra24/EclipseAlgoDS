1337e09e9ec8c94db9b24e23be82081df02f05af
Ram <r.kulkarni@ufl.edu>
2018-12-27 10:39:24 -0500

    Jump game - find if last index can be reached

--- AlgoCppPrac/src/CanJump.cpp
+++ AlgoCppPrac/src/CanJump.cpp
@@ -4,47 +4,26 @@ using namespace std;
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int i = 0, pri = 0;
-        while (i < (nums.size() - 1)) {
-            pri = i;
-            i += nums[i];
-            if (i >= (nums.size() - 1)) {
-                return true;
+        int toJump = nums.size() - 1;
+        int i = toJump - 1;
+        bool res = true;
+        while (i >= 0) {
+            if (nums[i] >= (toJump - i)) {
+                toJump = i;
+                res = true;
             }
-            if (nums[i] == 0) {
-                int start = i;
-                int end = start;
-                while (end < nums.size() && nums[end] == 0) {
-                    ++end;
-                }
-                int jump;
-                if (end == nums.size()) {
-                    // special case. just jump to the end.
-                    jump = end - start;
-                }
-                else {
-                    jump = end - start + 1;
-                }
-
-                int j;
-                for (j = pri; j < start; ++j) {
-                    if (nums[j] >= jump + start - j) {
-                        i = j + jump + start;
-                        break;
-                    }
-                }
-                if (j == start) {
-                    return false;
-                }
+            else {
+                res = false;
             }
+            --i;
         }
-        return true;
+        return res;
     }
 };
 
 int main() {
     Solution s;
-    vector<int> nums = {2, 5, 0, 0};
+    vector<int> nums = {3, 0};
     cout << s.canJump(nums);
     return 0;
 }
