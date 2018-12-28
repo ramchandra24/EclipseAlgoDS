0ffa6f9fb1c2f124e08406d8eaedc9d74891936e
Ram <r.kulkarni@ufl.edu>
2018-12-28 09:24:01 -0500

    Finding unique paths in matrix

--- AlgoCppPrac/src/UniquePath.cpp
+++ AlgoCppPrac/src/UniquePath.cpp
@@ -8,7 +8,7 @@ public:
         for (int i = 0; i < m; ++i) {
             mat[i][0] = 1;
         }
-        for (int i = 0; i < m; ++i) {
+        for (int i = 0; i < n; ++i) {
             mat[0][i] = 1;
         }
         for (int i = 1; i < m; ++i) {
@@ -16,12 +16,23 @@ public:
                 mat[i][j] = mat[i-1][j] + mat[i][j-1];
             }
         }
+        printmat(mat, m, n);
         return mat[m-1][n-1];
+    }
+
+    void printmat(int mat[][100], int m, int n) {
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                cout << mat[i][j] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
     }
 };
 
 int main() {
     Solution s;
-    cout << s.uniquePaths(7, 3) << endl;
+    cout << s.uniquePaths(3, 7) << endl;
     return 0;
 }
