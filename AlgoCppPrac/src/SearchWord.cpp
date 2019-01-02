c3a3849ab61a99c352ccb69c2213f8ef9fe3e3fc
Ram <r.kulkarni@ufl.edu>
2019-01-02 13:50:44 -0500

    Traverse Matrix - Left, Right, Down

---
+++ AlgoCppPrac/src/SearchWord.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        if (board.size() < 1) {
+            return false;
+        }
+        vector<vector<int>> vis(board.size(), vector<int>(board[0].size(), 0));
+        dfs(board, vis, 0, 0);
+        printMat(vis);
+        return false;
+    }
+
+    void dfs(vector<vector<char>> const& board, vector<vector<int>>& vis, int i, int j) {
+        //cout << "dfs on : " << i << " " << j << endl;
+        if (!visited(vis, i, j)) {
+            for (int k = 0 ; k < 3 ; ++k) {
+                int ni, nj;
+                int trn = 1 << k;
+                if (!(vis[i][j] & trn)) {
+                    vis[i][j] |= trn;
+                    tuple<int, int> turn = getNextNode(board, i, j, trn);
+                    ni = get<0>(turn);
+                    nj = get<1>(turn);
+                    if (-1 == ni || -1 == nj) {
+                        continue;
+                    }
+                    markTurn(vis, ni, nj, trn);
+                    printMat(vis);
+                    dfs(board, vis, ni, nj);
+                }
+            }
+        }
+    }
+
+    void markTurn(vector<vector<int>>& v, int i, int j, int turn) {
+        switch(turn) {
+        case 1:
+            v[i][j] |= 2;
+            break;
+        case 2:
+            v[i][j] |= 1;
+            break;
+        default :
+            break;
+        }
+    }
+
+    tuple<int, int> getNextNode(vector<vector<char>> const& board, int i, int j, int turn) {
+        tuple<int, int> nTurn(-1, -1);
+        switch(turn) {
+        case 1:
+            --j;
+            if (j < 0) {
+                return nTurn;
+            }
+            break;
+        case 2:
+            ++j;
+            if (j >= board[i].size()) {
+                return nTurn;
+            }
+            break;
+        case 4:
+            ++i;
+            if (i >= board.size()) {
+                return nTurn;
+            }
+            break;
+        default:
+            return nTurn;
+        }
+        return (make_tuple(i, j));
+    }
+
+    void markLeft(vector<vector<int>> &v, int i, int j) {
+        v[i][j] |= 1 << 0;
+    }
+    void markRight(vector<vector<int>> &v, int i, int j) {
+        v[i][j] |= 1 << 1;
+    }
+    void markDown(vector<vector<int>> &v, int i, int j) {
+        v[i][j] |= 1 << 2;
+    }
+
+    bool visited(vector<vector<int>> const &v, int i, int j) {
+        return (v[i][j] == 0x7);
+    }
+
+    template <typename T>
+    void printMat(vector<vector<T>> const &v) {
+        for (auto a : v) {
+            for (auto c : a) {
+                cout << c << " " ;
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+
+    vector<vector<char>> board =
+    {
+            {'A','B','C','E'},
+            {'S','F','C','S'},
+            {'A','D','E','E'}
+    };
+    Solution s;
+    s.printMat(board);
+    cout << s.exist(board, "SEED");
+
+    return 0;
+}
+
+
+
