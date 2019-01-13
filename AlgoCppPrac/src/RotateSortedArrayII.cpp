101cbd2bd2b906cb106cc652f56ae9b215b376dc
Ram <r.kulkarni@ufl.edu>
2019-01-13 15:22:52 -0500

    Log(n) rotated search with repetition

--- AlgoCppPrac/src/RotateSortedArrayII.cpp
+++ AlgoCppPrac/src/RotateSortedArrayII.cpp
@@ -9,45 +9,29 @@ public:
 
 
     bool searchVec(vector<int>& nums, int l, int r, int target) {
-        bool res = false;
         while (l <= r) {
             int mid = l + (r - l) / 2;
             if (nums[mid] == target) {
                 return true;
             }
-            if (nums[mid] < target) {
-                if (nums[l] != nums[r]) {
-                    // look left
-                    if (nums[r] < target) {
-                        r = mid - 1;
-                    }
-                    else {
-                        l = mid + 1;
-                    }
+            if (nums[mid] < nums[l]) {
+                if (target > nums[mid] && target <= nums[r]) {
+                    l = mid + 1;
                 }
                 else {
-                    res |= searchVec(nums, l, mid - 1, target);
-                    res |= searchVec(nums, mid + 1, r, target);
-                    return res;
+                    r = mid - 1;
                 }
             }
-            else if (nums[mid] > target) {
-# if 0
-                if (nums[l] != nums[r]) {
-                   // look left
-                   if (nums[l] <= target || nums[r] > target) {
-                       r = mid - 1;
-                   }
-                   else {
-                       l = mid + 1;
-                   }
-               }
-# endif
-         //      else {
-                   res |= searchVec(nums, l, mid - 1, target);
-                   res |= searchVec(nums, mid + 1, r, target);
-                   return res;
-       //        }
+            else if (nums[mid] > nums[l]) {
+                if (target < nums[mid] && target >= nums[l]) {
+                    r = mid - 1;
+                }
+                else {
+                    l = mid + 1;
+                }
+            }
+            else {
+                ++l;
             }
         }
         return false;
