657ad42476f8b583548e211da5d1060ef351b09f
Ram <r.kulkarni@ufl.edu>
2019-01-13 19:55:25 -0500

    Recursive solution

--- AlgoCppPrac/src/RemoveDuplicatesLL.cpp
+++ AlgoCppPrac/src/RemoveDuplicatesLL.cpp
@@ -10,72 +10,21 @@ struct ListNode {
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-
-        ListNode* fir, *sec;
-        if (head->next) {
-            fir = head->next;
-        }
-        else {
+        if (!head || !head->next) {
             return head;
         }
-        if (fir->next) {
-            sec = fir->next;
-        }
-        else {
-            if (head->val == fir->val) {
-                head->next = NULL;
-            }
+        ListNode* cur = head->next;
+        int v = head->val;
+        if (v != cur->val) {
+            head->next = deleteDuplicates(cur);
             return head;
         }
-        ListNode* curHead = head;
-        while (curHead->val == fir->val) {
-            curHead = curHead->next;
-        }
-        head = curHead;
-        fir = curHead->next;
-        if (fir) {
-            sec = fir->next;
-        }
         else {
-            return head;
-        }
-        while (sec) {
-            if (fir->val == sec->val) {
-                fir = sec->next;
-                if (fir) {
-                    sec = fir->next;
-                }
+            while (cur && v == cur->val) {
+                cur = cur->next;
             }
-            else {
-                head->next = fir;
-                head = head->next;
-                fir = fir->next;
-                sec = sec->next;
-            }
-
-#if 0
-            if (sec->val != fir->val) {
-                if (fir->next == sec) {
-                    head->next = fir;
-                }
-                else {
-                    head->next = sec;
-                }
-                head = head->next;
-                //fir = sec;
-            }
-            fir = sec;
-            sec = sec->next;
-#endif
+            return deleteDuplicates(cur);
         }
-        if (fir) {
-            head->next = fir;
-            head = head->next;
-        }
-        if (head->next) {
-            head->next = NULL;
-        }
-        return curHead;
     }
 
     void printNodes(ListNode* head) {
@@ -91,18 +40,18 @@ public:
 
 int main() {
     ListNode* head = new ListNode(1);
-    ListNode* a = new ListNode(2);
-    ListNode* b = new ListNode(3);
-    ListNode* c = new ListNode(3);
-    ListNode* d = new ListNode(4);
-    ListNode* e = new ListNode(4);
-    ListNode* f = new ListNode(5);
+    ListNode* a = new ListNode(1);
+    ListNode* b = new ListNode(1);
+    ListNode* c = new ListNode(2);
+    ListNode* d = new ListNode(3);
+    ListNode* e = new ListNode(2);
+    ListNode* f = new ListNode(2);
 
     head->next = a;
     a->next = b;
     b->next = c;
     c->next = d;
-    d->next = e;
+    //d->next = e;
     e->next = f;
 
     Solution s;
